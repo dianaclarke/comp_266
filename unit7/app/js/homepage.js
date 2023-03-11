b210b13b1b9571a0ab1eaacc820fe43785964bd2
Diana Clarke <diana.joan.clarke@gmail.com>
2023-03-11 11:33:51 -0700

    netlify

--- unit7/app/js/homepage.js
+++ unit7/app/js/homepage.js
@@ -1,4 +1,27 @@
 //
+// On homepage load, sey hello to OpenAI chat.
+//
+$(function() {
+    //const question = 'Create me an anatomy 101 quiz with 50 questions. Format the questions and answers in json, make options a dictionary, answer keyed by index.';
+    const question = 'Hello again!';
+    openai(question).then((data) => {
+        const answer = data['choices'][0]['message']['content'];
+        console.log(answer);
+        $('#ai').text(answer);
+    });
+});
+
+
+//
+// Using Netlify functions, query OpenAI.
+//
+async function openai(question) {
+    const response = await fetch(`/.netlify/functions/open-ai/open-ai.js?question=${question}`);
+    return response.json();
+};
+
+
+//
 // Make the entire chapter box on the homepage clickable, not just the link.
 //
 $(function() {
@@ -12,7 +35,7 @@ $(function() {
 
 
 //
-// On homepage load, enable "learn how" accordion.
+// On homepage load, enable 'learn how' accordion.
 //
 // https://jqueryui.com/accordion/#collapsible
 //
